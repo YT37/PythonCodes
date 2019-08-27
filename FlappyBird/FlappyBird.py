94fc6e76cbc021c944c85a6c3f4d08a54aedbfa5
Yug Thapar <yugthapar37@gmail.com>
2019-08-28 00:25:59 +0530

    Added Codes

--- FlappyBird/FlappyBird.py
+++ FlappyBird/FlappyBird.py
@@ -6,9 +6,12 @@ import os
 
 winWidth = 500
 winHeight = 800
+gen = 0
+pg.font.init()
+statFont = pg.font.SysFont("comicsans", 50)
 birdImg = [
     pg.transform.scale2x(
-        pg.image.load("D:\\Codes\\PythonCodes\\FlappyBird\\Imgs\\Bird.png")
+        pg.image.load("D:\\Codes\\PythonCodes\\FlappyBird\\Imgs\\Bird1.png")
     ),
     pg.transform.scale2x(
         pg.image.load("D:\\Codes\\PythonCodes\\FlappyBird\\Imgs\\Bird2.png")
@@ -26,8 +29,6 @@ pipeImg = pg.transform.scale2x(
 bgImg = pg.transform.scale2x(
     pg.image.load("D:\\Codes\\PythonCodes\\FlappyBird\\Imgs\\BG.png")
 )
-pg.font.init()
-statFont = pg.font.SysFont("comicsans", 50)
 
 
 class Bird:
@@ -53,12 +54,12 @@ class Bird:
 
     def move(self):
         self.tickCou += 1
-        disp = self.vel * self.tickCou + 1.5 * self.tickCou ** 2
+        disp = self.vel*(self.tickCou) + 0.5*(3)*(self.tickCou)**2
         if disp >= 16:
-            disp = 16
+            disp = (disp/abs(disp)) * 16
         if disp < 0:
             disp -= 2
-        self.y = self.y * disp
+        self.y = self.y + disp
         if disp < 0 or self.y < self.height + 50:
             if self.tilt < self.maxRot:
                 self.tilt = self.maxRot
@@ -81,11 +82,10 @@ class Bird:
         if self.tilt <= -80:
             self.img = self.bImg[1]
             self.imgCou = self.animaTime * 2
-        rotImg = pg.transform.rotate(self.img, self.tilt)
-        rect = rotImg.get_rect(
-            center=self.img.get_rect(topleft=(self.x, self.y)).center
-        )
-        win.blit(rotImg, rect.topleft)
+        if self.tilt <= -80:
+            self.img = self.bImg[1]
+            self.imgCou = self.animaTime * 2
+        blitRotateCenter(win, self.img, (self.x, self.y), self.tilt)
 
     def mask(self):
         return pg.mask.from_surface(self.img)
@@ -123,9 +123,9 @@ class Pipes:
         bottomMask = pg.mask.from_surface(self.pipeBottom)
         topOffset = (self.x - bird.x, self.top - round(bird.y))
         bottomOffset = (self.x - bird.x, self.bottom - round(bird.y))
-        tPoint = birdMask.overlap(topMask, topOffset)
         bPoint = birdMask.overlap(bottomMask, bottomOffset)
-        if tPoint or bPoint:
+        tPoint = birdMask.overlap(topMask, topOffset)
+        if bPoint or  tPoint:
             return True
         return False
 
@@ -153,25 +153,49 @@ class Base:
         win.blit(self.img, (self.x1, self.y))
 
 
-def drawWin(win, bird, base, pipes, score):
+def blitRotateCenter(surf, image, topleft, angle):
+    rotated_image = pg.transform.rotate(image, angle)
+    new_rect = rotated_image.get_rect(center=image.get_rect(topleft=topleft).center)
+    surf.blit(rotated_image, new_rect.topleft)
+
+
+def drawWin(win, birds, base, pipes, score, genn):
+    if genn == 0:
+        genn = 1
     win.blit(bgImg, (0, 0))
     for pipe in pipes:
         pipe.draw(win)
     base.draw(win)
-    bird.draw(win)
+    for bird in birds:
+        bird.draw(win)
     text = statFont.render("Score: " + str(score), 1, (255, 255, 255))
     win.blit(text, (winWidth - 10 - text.get_width(), 10))
+    text = statFont.render("Gens: " + str(genn), 1, (255, 255, 255))
+    win.blit(text, (10, 10))
+    text = statFont.render("Alive: " + str(len(birds)),1,(255,255,255))
+    win.blit(text, (10, 50))
     pg.display.update()
 
 
-def main():
-    bird = Bird(230, 350)
+def main(genomes, config):
+    global gen
+    gen += 1
+    nets = []
+    gens = []
+    birds = []
     base = Base(730)
     pipes = [Pipes(600)]
     win = pg.display.set_mode((winWidth, winHeight))
     run = True
     score = 0
     clock = pg.time.Clock()
+    for _, g in genomes:
+        g.fitness = 0
+        net = neat.nn.FeedForwardNetwork.create(g, config)
+        nets.append(net)
+        birds.append(Bird(230, 350))
+        gens.append(g)
+
     while run:
         clock.tick(30)
         rem = []
@@ -179,26 +203,78 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 run = False
-        if bird.y + bird.img.get_height() >= 730:
-            pass
+                pg.quit()
+                quit()
+                break
+        pipeCount = 0
+        if len(birds) > 0:
+            if (
+                len(pipes) > 1
+                and birds[0].x > pipes[0].x + pipes[0].pipeTop.get_width()
+            ):
+                pipeCount = 1
+        for x, bird in enumerate(birds):
+            bird.move()
+            gens[x].fitness += 0.1
+            output = nets[x].activate(
+                (
+                    bird.y,
+                    abs(bird.y - pipes[pipeCount].height),
+                    abs(bird.y - pipes[pipeCount].bottom),
+                )
+            )
+            if output[0] > 0.5:
+                bird.jump()
+
         for pipe in pipes:
-            if pipe.collide(bird):
-                print("Collided")
+            for x, bird in enumerate(birds):
+                if pipe.collide(bird):
+                    gens[x].fitness -= 1
+                    birds.pop(x)
+                    nets.pop(x)
+                    gens.pop(x)
+
+                if bird.y + bird.img.get_height() >= 730:
+                    birds.pop(x)
+                    nets.pop(x)
+                    gens.pop(x)
+
+                if not pipe.passed and pipe.x < bird.x:
+                    pipe.passed = True
+                    addPipe = True
+
             if pipe.x + pipe.pipeTop.get_width() < 0:
                 rem.append(pipe)
-            if not pipe.passed and pipe.x < bird.x:
-                pipe.passed = True
-                addPipe = True
             pipe.move()
+
         if addPipe:
+            for g in gens:
+                g.fitness += 5
             score += 1
             pipes.append(Pipes(600))
+
         for r in rem:
             pipes.remove(r)
         base.move()
-        drawWin(win, bird, base, pipes, score)
-    pg.quit()
-    quit()
+        drawWin(win, birds, base, pipes, score, gen)
+        
+def run(path):
+    config = neat.config.Config(
+        neat.DefaultGenome,
+        neat.DefaultReproduction,
+        neat.DefaultSpeciesSet,
+        neat.DefaultStagnation,
+        path,
+    )
+    popu = neat.Population(config)
+    stats = neat.StatisticsReporter()
+    popu.add_reporter(neat.StdOutReporter(True))
+    popu.add_reporter(stats)
+    winner = popu.run(main, 50)
+    print('\nBest genome:\n{!s}'.format(winner))
 
 
-main()
+if __name__ == "__main__":
+    dir = os.path.dirname(__file__)
+    path = os.path.join(dir, "config.txt")
+    run(path)
