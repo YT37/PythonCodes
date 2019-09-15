797ee59336b268d70e70feade10d12f32eec14d7
Yug Thapar <yugthapar37@gmail.com>
2019-09-16 03:31:54 +0530

    Added & Refined Codes

--- AI/AI.py
+++ AI/AI.py
@@ -3,15 +3,17 @@ from __future__ import print_function
 import datetime
 import os.path
 import pickle
+import subprocess
 import time
 
 import playsound as ps
 import pyaudio as pa
+import pyttsx3
+import pytz
 import speech_recognition as sr
 from google.auth.transport.requests import Request
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
-from gtts import gTTS as t
 
 scopes = ["https://www.googleapis.com/auth/calendar.readonly"]
 months = [
@@ -30,53 +32,82 @@ months = [
 ]
 days = ["Monday", "Tuesday", "Wednesday", "Thurday", "Friday", "Saturday", "Sunday"]
 daysExt = ["st", "nd", "rd", "th"]
+calStrs = ["What do i have", "do i have plans", "am i busy"]
+noteStrs = ["make a note", "write this down", "remember this"]
 
 
 def authenticate():
     creds = None
+
     if os.path.exists("Token.pickle"):
         with open("Token.pickle", "rb") as Token:
             creds = pickle.load(Token)
+
     if not creds or not creds.valid:
+
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
+
         else:
             flow = InstalledAppFlow.from_client_secrets_file("Cred.json", scopes)
             creds = flow.run_local_server(port=0)
+
         with open("Token.pickle", "wb") as Token:
             pickle.dump(creds, Token)
+
     service = build("calendar", "v3", credentials=creds)
+
     return service
 
 
-def getevent(n, s):
-    now = datetime.datetime.utcnow().isoformat() + "Z"
-    print(f"Getting the upcoming {n} events")
-    events_result = (
+def getevent(days, s):
+    date = datetime.datetime.combine(days, datetime.datetime.min.time())
+    end = datetime.datetime.combine(days, datetime.datetime.max.time())
+
+    utc = pytz.UTC
+
+    date = date.astimezone(utc)
+    end = end.astimezone(utc)
+
+    eventResult = (
         s.events()
         .list(
             calendarId="primary",
-            timeMin=now,
-            maxResults=n,
+            timeMin=date.isoformat(),
+            timeMax=end.isoformat(),
             singleEvents=True,
             orderBy="startTime",
         )
         .execute()
     )
-    events = events_result.get("items", [])
+
+    events = eventResult.get("items", [])
 
     if not events:
         print("No upcoming events found.")
-    for event in events:
-        start = event["start"].get("dateTime", event["start"].get("date"))
-        print(start, event["summary"])
+
+    else:
+        speak(f"You have {len(events)} Events On This Day.")
+
+        for event in events:
+            start = event["start"].get("dateTime", event["start"].get("date"))
+            print(start, event["summary"])
+            startTime = str(start.split("T")[1].split("-")[0])
+
+            if int(startTime.split(":")[0]) < 12:
+                startTime = startTime + "AM"
+
+            else:
+                startTime = str(int(startTime.split(":")[0]) - 12)
+                startTime = startTime + "PM"
+
+            speak(event["summary"] + "At" + startTime)
 
 
 def speak(text):
-    tts = t(text=text, lang="en")
-    fn = "voice.mp3"
-    tts.save(fn)
-    ps.playsound(fn)
+    engine = pyttsx3.init()
+    engine.say(text)
+    engine.runAndWait()
 
 
 def audio():
@@ -107,12 +138,12 @@ def getDate(text):
 
     day = -1
     dayWeek = -1
-    month = -1
+    mon = -1
     year = today.year
 
     for word in text.split():
         if word in months:
-            month = months.index(word) + 1
+            mon = months.index(word) + 1
 
         elif word in days:
             dayWeek = days.index(word)
@@ -131,13 +162,17 @@ def getDate(text):
                     except:
                         pass
 
-    if month < today.month and month != -1:
-        year += 1
+    if mon < today.month and mon != -1:
+        year = year + 1
 
-    if day < today.day and month == -1 and day != -1:
-        month += 1
+    if mon == -1 and day != -1:
+        if day < today.day:
+            mon = today.month + 1
 
-    if month == -1 and day == -1 and dayWeek != -1:
+        else:
+            mon = today.month
+
+    if mon == -1 and day == -1 and dayWeek != -1:
         currentDayWeek = today.weekday()
         dif = dayWeek - currentDayWeek
 
@@ -148,9 +183,37 @@ def getDate(text):
                 dif += 7
 
         return today + datetime.timedelta(dif)
+    if day != -1:
+        return datetime.date(month=mon, day=day, year=year)
 
-    return datetime.date(month=month, day=day, year=year)
+
+def note(text):
+    date = datetime.datetime.now()
+    fileName = str(date).replace(":", "-") + "-note.txt"
+
+    with open(fileName, "w") as f:
+        f.write(text)
+
+    subprocess.Popen(["notepad.exe", fileName])
 
 
-text = audio().lower()
-print(getDate(text))
+service = authenticate()
+print("Start")
+text = audio()
+
+for phrase in calStrs:
+    if phrase in text:
+        date = getDate(text)
+
+        if date:
+            getevent(date, service)
+
+        else:
+            speak("Please Try Again")
+
+for phrase in noteStrs:
+    if phrase in text:
+        speak("What Would You Like Me To Write Down ?")
+        noteText = audio().lower()
+        note(noteText)
+        speak("I've Made A Note Of That.")
