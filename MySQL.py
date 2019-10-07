8e946d1bc5f32ef7445bb7acece9016cf7678487
Yug Thapar <yugthapar37@gmail.com>
2019-10-08 04:26:37 +0530

    Added & Refined Codes

--- MySQL.py
+++ MySQL.py
@@ -1,7 +1,30 @@
 import mysql.connector
+from datetime import datetime
 
 db = mysql.connector.connect(
     host="localhost", user="root", passwd="Agasthya4572$:my", database="Test"
 )
 
 cursor = db.cursor()
+
+# cursor.execute(
+#     "CREATE TABLE Info (Name varchar(50) NOT NULL , Created datetime NOT NULL, Gender ENUM('M', 'F', 'O'),id int PRIMARY KEY NOT NULL AUTO_INCREMENT)"
+# )
+
+
+# cursor.execute(
+#     "INSERT INTO Info (Name,Created,Gender) VALUES (%s,%s,%s)",
+#     ("Krish", datetime.now(), "M"),
+# )
+
+# db.commit()
+
+# cursor.execute("SELECT Id,Name FROM Info ORDER BY Id ASC")
+
+# for x in cursor:
+#     print(x)
+
+# cursor.execute("ALTER TABLE Info ADD COLUMN Food VARCHAR(50) NOT NULL")
+# cursor.execute("DESCRIBE Info")
+
+# cursor.execute("ALTER TABLE Info CHANGE Name firstName VARCHAR(10)")
