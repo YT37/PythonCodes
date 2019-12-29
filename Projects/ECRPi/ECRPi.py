77706bd568a3dedc0d1b6f6b08caf1ab57d79ea7
Yug Thapar <yugthapar37@gmail.com>
2019-12-30 03:21:53 +0530

    Updated ECRPi Project

--- Projects/ECRPi/ECRPi.py
+++ Projects/ECRPi/ECRPi.py
@@ -7,13 +7,13 @@ it = pyfirmata.util.Iterator(board)
 it.start()
 
 LM35 = board.get_pin("a:0:i")
-EC = board.get_pin("a:1:i")
-print("Now startting")
+EC = board.get_pin("a:2:i")
+print("Now Starting")
 time.sleep(30)
 print("Running")
+
 LM35Read = LM35.read()
 ECRead = EC.read()
 
 print(f"Temp :- {int(str(LM35Read)[2:])/10}")
-print(f"Conductivity :- {str(ECRead)[2:]}")
-
+print(f"Conductivity :- {ECRead}")
