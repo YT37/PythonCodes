3aa8f603cf6aa7a4f6ccefc46fcf43cc0cc2dc87
Yug Thapar <46230146+YT37@users.noreply.github.com>
2020-03-29 22:54:49 +0530

    Create README.py

---
+++ Snippets/TreeMaker/README.py
@@ -0,0 +1,15 @@
+# TreeMaker
+- This project makes trees of the chosen size in Python.
+
+# Requirements
+ - Python 3.6 or up
+ 
+ # Instructions To Use
+    - **Windows**
+   ```
+   python TreeMaker.py
+   ```
+ - **Mac or Linux**
+   ```
+   python3 TreeMaker.py
+   ```
