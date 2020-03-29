e04d5ca04cb27ad61f8bc12fb398158fd6a2061b
Yug Thapar <46230146+YT37@users.noreply.github.com>
2020-03-29 22:53:32 +0530

    Create README.py

---
+++ Snippets/MessageEncrypter/README.py
@@ -0,0 +1,15 @@
+# MessageEncrypter
+- This project encrypts/decrypts messages in Python.
+
+# Requirements
+ - Python 3.6 or up
+ 
+ # Instructions To Use
+    - **Windows**
+   ```
+   python MessageEncrypter.py
+   ```
+ - **Mac or Linux**
+   ```
+   python3 MessageEncrypter.py
+   ```
